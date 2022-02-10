d03390919326e20d3e41c480c8594c51d1062e89
Cefas88136 <88136@glr.nl>
2022-02-10 11:02:33 +0100

    -

--- js/quiz2.js
+++ js/quiz2.js
@@ -1,7 +1,19 @@
 questionOrder = [questions[0], questions[1]];
 
 function showQuestion(i) {
-    console.log(questionOrder[i])
-    document.getElementById('questionBox').innerText = questionOrder[i]['question'];
+    let question = questionOrder[i]
+    console.log(question);
+    document.getElementById('questionBox').innerText = question['question'];
+
+    let inenerHTML = '';
+    if (question['type'] == 'multipleChoice') {
+        for (q in question['options']) {
+            console.log(question['options'][q])
+            inenerHTML += `
+            <button onclick="checkAnswer" class="mcOption">${question['options'][q]}</button>
+            `
+        }
+    }
+    document.getElementById('answers').innerHTML = inenerHTML
 }
 showQuestion(0);
--- js/quiz2_vragen.js
+++ js/quiz2_vragen.js
@@ -1,11 +1,11 @@
 const q1 = {
-    qType: 'multipleChoice',
+    type: 'multipleChoice',
     question: 'This is the first question',
     options: ['option 1', 'option 2', 'option 3', 'option 4'],
     answer: 2
 }
 const q2 = {
-    qType: 'multipleChoice',
+    type: 'multipleChoice',
     question: 'This is question 2',
     options: ['dddfe', 'ade', 'hggrea', 'asdfeeefasdeef'],
     answer: 2
