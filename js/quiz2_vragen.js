642fc2916fb108445a543618fb4bd6664d9be1b5
Cefas88136 <88136@glr.nl>
2022-02-10 10:33:37 +0100

    preBeroeps

---
+++ js/quiz2_vragen.js
@@ -0,0 +1,13 @@
+let questions = [q1, q2]
+let q1 = {
+    qType: 'multipleChoice',
+    question: 'this is the first question',
+    options: ['option 1', 'option 2', 'option 3', 'option 4'],
+    answer: 2
+}
+let q2 = {
+    qType: 'multipleChoice',
+    question: 'this is question 2',
+    options: ['dddfe', 'ade', 'hggrea', 'asdfeeefasdeef'],
+    answer: 2
+}
