ad6e23802303866e53a829825ff8ee54095cac0d
QuillanW <56843492+QuillanW@users.noreply.github.com>
2022-02-22 12:13:00 +0100

    update

--- js/quiz1.js
+++ js/quiz1.js
@@ -1,3 +1,4 @@
+"use strict"
 function select(selected) {
     for (let i = 1; i < 11; i++) {
         document.getElementById('box'+i).style.display = 'none';
@@ -8,14 +9,58 @@ function select(selected) {
 
 let answers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
-function answer(correct) {
-  answered = (event.target.parentElement.parentElement.id).replace("box","") - 1;
+function makeQuestions() {
+  var container = document.getElementById('questions');
+  for (let i = 1; i < 11; i++) {
+    var newQuestion = document.createElement('div');
+    newQuestion.id = 'box' + i;
+    newQuestion.classList.add('contentBox');
+    var newQuestionQN = document.createElement('h2');
+    newQuestionQN.innerHTML = 'Vraag ' + i;
+    newQuestion.appendChild(newQuestionQN);
+    var newQuestionQ = document.createElement('h3');
+    newQuestionQ.innerHTML = questions[i-1]['question'];
+    newQuestion.appendChild(newQuestionQ);
+    var newQuestionAB = document.createElement('div');
+    newQuestionAB.id = 'answers'
+    for (let l = 1; l < 5; l++) {
+      var newQuestionA = document.createElement('button');
+      newQuestionA.classList.add('answerButton');
+      newQuestionA.id = 'answer' + l;
+      newQuestionA.innerHTML = questions[i-1]['options'][l-1]
+      newQuestionA.addEventListener('click', answer);
+      newQuestionAB.appendChild(newQuestionA);
+    }
+    newQuestion.appendChild(newQuestionAB);
+    container.appendChild(newQuestion);
+  };
+
+  console.log('Questions created!');
+}
+
+function answer() {
+  var answered = (event.target.parentElement.parentElement.id).replace("box","") - 1;
+  var answered1 = (event.target.id).replace('answer', '');
+  let correct = false;
+
+  if (answered1 == (questions[answered]['answer'])) {
+    correct = true
+  } else {
+    correct = false
+  }
   if (correct == true) {
     answers.splice(answered, 1, 1)
+    document.getElementById('corrector' + (answered + 1)).classList.remove('incorrect')
+    document.getElementById('corrector' + (answered + 1)).classList.add('correct')
   } else if (correct == false) {
     answers.splice(answered, 1, 0)
+    document.getElementById('corrector' + (answered + 1)).classList.remove('correct')
+    document.getElementById('corrector' + (answered + 1)).classList.add('incorrect')
   }
-  button = 'btn' + (answered + 1)
+
+console.log(answered, correct)
+
+  var button = 'btn' + (answered + 1)
   document.getElementById(button).classList.add('answered')
 }
 
---
+++ js/quiz1_vragen.js
@@ -0,0 +1,52 @@
+const questions = [
+    {
+        question: 'Hoe heette de eerste Farming simulator?',
+        options: ['Farming Simulator', 'Farming Simulator 1', 'Farming Simulator 11', 'Farming Simulator 2011'],
+        answer: 4
+    },
+    {
+        question: 'Hoe heet het bedrijf dat farming simulator heeft gemaakt?',
+        options: ['SCS Software' , 'Farming Simulator' , 'Giants Software' , 'Steel Wool Studios'],
+        answer: 3
+    },
+    {
+        question: 'Op wat voor apparaten kon je Farming Simulator 14 spelen?',
+        options: ['Xbox' , 'Mobiel' , 'PC' , 'Playstation'],
+        answer: 2
+    },
+    {
+        question: 'In welke versie waren er voor het eerst apparaten te vinden van John Deere?',
+        options: ['Farming Simulator 13' , 'Farming Simulator 15' , 'Farming Simulator 17' , 'Farming Simulator 19'],
+        answer: 4
+    },
+    {
+        question: 'Wat was de grootste traktor die te vinden was in (Basegame) Farming Simulator 19?',
+        options: ['Case Quadtrack', 'John Deere 9RX', 'Big Bud 747', 'Claas Xerion'],
+        answer: 1
+    },
+    {
+        question: 'Welke grote verandering was heel duidelijk te zien tussen Farming Simulator 17 en 19?',
+        options: ['De traktor kwaliteit', 'De functionaliteit van dieren', 'De graphics', 'Het realisme'],
+        answer: 3
+    },
+    {
+        question: 'Welk soort planten was toegevoegd in Farming Simulator 22?',
+        options: ['Aardappelen', 'Wortels', 'Sla', 'Oliezaad radijs'],
+        answer: 3
+    },
+    {
+        question: 'Welk vrachtwagenmerk is toegevoegd aan Farming Simulator 22?',
+        options: ['Mack', 'Peterbilt', 'MAN', 'Daf'],
+        answer: 1
+    },
+    {
+        question: 'Welk voertuig kreeg je gratis als je de Farming Simulator 22 pre-orderde?',
+        options: ['John Deere 9RX', 'Claas Xerion Saddletrac', 'Custom pickup truck', 'New Holland T9'],
+        answer: 2
+    },
+    {
+        question: 'Wat was de naam van het evenement dat jaarlijks word georganizeerd voor Farming Simulator?',
+        options: ['FS Battle Royale', 'Farming Battle', 'Farming Simulator Tournament', 'Farming Simulator League'],
+        answer: 4
+    },
+]
